7cbbaff4578c7218400af165f907412c52786f80
Jamezfn <jamesmakachia19@gmail.com>
2025-11-03 22:18:23 +0300

    Query parameters

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-
+from typing import Optional
 app = FastAPI()
 
 @app.get('/')
@@ -9,6 +9,12 @@ def index():
 @app.get('/about')
 def about():
     return {"data": "about page"}
+
+@app.get('/blog')
+def index(limit=10, published: bool = True, sort: Optional[str] = None):
+    if published:
+        return {"data": f'{limit} published blogs from the db'}
+    return {"data": f'{limit} blogs from the db'}
 
 @app.get('/blog/{id}')
 def show(id: int):
