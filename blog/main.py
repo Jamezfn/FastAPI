5623cacc17609a8142184bc663de3a26890421ec
Jamezfn <jamesmakachia19@gmail.com>
2025-11-04 14:52:09 +0300

    CRUD operations

---
+++ blog/main.py
@@ -0,0 +1,52 @@
+from fastapi import FastAPI, Depends, status, Response, HTTPException
+import schemas, models
+from dB import engine, sessionLocal
+from sqlalchemy.orm import Session
+
+
+app = FastAPI()
+models.Base.metadata.create_all(bind=engine)
+
+def get_db():
+    db = sessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+@app.post('/blog', status_code=status.HTTP_201_CREATED)
+def create(request: schemas.Blog, db: Session = Depends(get_db)):
+    new_blog = models.Blog(title=request.title, body=request.body)
+    db.add(new_blog)
+    db.commit()
+    db.refresh(new_blog)
+
+    return new_blog
+@app.delete('/blog/{id}', status_code=status.HTTP_204_NO_CONTENT)
+def delete(id: int, db: Session = Depends(get_db)):
+    blog = db.query(models.Blog).filter(models.Blog.id == id)
+    if not blog.first():
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f'Blog with id {id} not found')
+    db.commit()
+    return Response(status_code=status.HTTP_204_NO_CONTENT)
+
+@app.put('/blog/{id}', status_code=status.HTTP_202_ACCEPTED)
+def update(id: int, request: schemas.Blog, db: Session = Depends(get_db)):
+    blog = db.query(models.Blog).filter(models.Blog.id == id)
+    if not blog.first():
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f'Blog with id {id} not found')
+    
+    blog.update(request.dict())
+    db.commit()
+    return blog.first()
+
+@app.get('/blog', status_code=status.HTTP_200_OK)
+def get_all(db: Session = Depends(get_db)):
+    return db.query(models.Blog).all()
+
+@app.get('/blog/{id}', status_code=status.HTTP_200_OK)
+def get(id: int, db: Session = Depends(get_db)):
+    blog = db.query(models.Blog).filter(models.Blog.id == id).first()
+    if not blog:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f'Blog with id {id} not found')
+    return blog
